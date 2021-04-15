c89e33903cf18a742881c01446cc313f8724c643
ironman <charlesli.huang@gmail.com>
2021-04-15 10:41:07 +0200

    added index file for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :patients
+  root to: "users#index"
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
